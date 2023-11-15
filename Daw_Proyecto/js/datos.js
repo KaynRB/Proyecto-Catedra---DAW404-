46d260617e4a0cc01647c617b42f07fc778e1409
RB230288 <125234995+KaynRB@users.noreply.github.com>
2023-11-15 08:39:17 -0600

    Delete Daw_Proyecto directory

--- Daw_Proyecto/js/datos.js
+++
@@ -1,9 +0,0 @@
-function hideLabel(selectElement) {
-  const label = selectElement.previousElementSibling;
-  if (label && selectElement.value) {
-    label.style.display = 'none';
-  } else {
-    label.style.display = 'block';
-  }
-}
-
